fc2736a0eba2a4852bd59d4da3b9af5eea2c7a66
hyc1743 <hyc1743@users.noreply.github.com>
2026-03-02 13:40:10 +0800

    chore: narrow API whitelist to bitget gate htx backpack

--- targets.js
+++ targets.js
@@ -1,16 +1,9 @@
 export const TARGET_ORIGINS = [
-  "https://api-aws.huobi.pro",
   "https://api.backpack.exchange",
-  "https://api.binance.com",
   "https://api.bitget.com",
-  "https://api.bybit.com",
   "https://api.gateio.ws",
   "https://api.hbdm.vn",
-  "https://api.hyperliquid.xyz",
-  "https://fapi.asterdex.com",
-  "https://fapi.binance.com",
-  "https://sapi.asterdex.com",
-  "https://www.okx.com"
+  "https://api-aws.huobi.pro"
 ];
 
 export const ALLOWED_PAGE_ORIGINS = [
